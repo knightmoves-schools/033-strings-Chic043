cf4d05a93f5fafa4580b642c860441e93987264b
CHIC043 <karsen.kapayou@students.msswarriors.org>
2026-04-16 18:38:26 +0000

    check

---
+++ index.js
@@ -0,0 +1,7 @@
+let firstName = "Avery";
+let jobTitle = "Web Developer";
+let city = "Seattle";
+
+let newJobPost = `${firstName} is hiring for a ${jobTitle} role in ${city}.`;
+
+document.getElementById("result").innerHTML = newJobPost;
